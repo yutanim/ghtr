1876f7d65b74b18866975949fc58245923eb3716
yutanim <yuta.tanimura425@gmail.com>
2016-05-18 18:31:17 +0100

    repo用のstruct作成

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	//"fmt"
+	"github.com/PuerkitoBio/goquery"
+	//"io"
 )
 
+type Repo struct {
+	Name        string
+	Language    string
+	Owner       string
+	Description string
+	StarNum     int
+}
+
+type Result struct {
+	repos []Repo
+}
+
+func (r *Repo) Setter(name, language, owner, description string, starnum int) {
+	r.Name = name
+	r.Language = language
+	r.Owner = owner
+	r.Description = description
+	r.StarNum = starnum
+}
+
 func main() {
-	url := "http://github-trends.ryotarai.info/rss/github_trends"
+	url := createURL("swift", "today")
+	results := fetchPage(url)
+}
 
-	hoge := "hogee"
-	req, _ := http.NewRequest("GET", url, nil)
-	client := new(http.Client)
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
+func fetchPage(url string) Result {
+	res := Result{}
+	var repos []Repo
+	doc, _ := goquery.NewDocument(url)
 
-	fmt.Println(hoge)
+	res.repos = repos
+	return res
+}
+
+func createURL(language, since string) string {
+	url := "https://github.com/trending"
+	if language != "" {
+		url += "/" + language
+	}
+	if since != "" {
+		url += "?since=" + since
+	}
+
+	return url
 }
