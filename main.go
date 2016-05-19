20f43bc38fffa515a312b728ec1bdcef3b1251e5
yutanim <yuta.tanimura425@gmail.com>
2016-05-19 08:03:20 +0100

    fetchしてownerとrepoの名前取るところまで

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	//"io"
+	"strings"
 )
 
 type Repo struct {
@@ -29,13 +29,19 @@ func (r *Repo) Setter(name, language, owner, description string, starnum int) {
 func main() {
 	url := createURL("swift", "today")
 	results := fetchPage(url)
+	fmt.Println(results)
 }
 
 func fetchPage(url string) Result {
 	res := Result{}
 	var repos []Repo
 	doc, _ := goquery.NewDocument(url)
-
+	doc.Find(".repo-list-item").Each(func(i int, item *goquery.Selection) {
+		path := item.Find(".repo-list-name a").Text()
+		s := strings.Split(path, "/")
+		owner, name := s[0], s[1]
+		fmt.Println(owner, name)
+	})
 	res.repos = repos
 	return res
 }
